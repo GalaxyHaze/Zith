fa139e3e32342b06939bde822b5c7b5bed7334da
diogo <diogoalexandredecodasilvapt@gmail.com>
2026-03-28 13:35:17 +0000

    refined content

--- impl/main.cpp
+++ impl/main.cpp
@@ -1,13 +1,16 @@
 #ifdef KALIDOUS_WASM
 #include <iostream>
-    int main() {
-        std::cout << "Thank you for installing Kalidous, however this version is opaque for WASM rigth now\n";
-        std::cout << "Im doing my best in order to have more things to show, WASM will come soon";
-        return -1;
-    }
+
+int main() {
+    std::cout << "Thank you for installing Kalidous." << std::endl;
+    std::cout << "The WASM version is currently under development and opaque." << std::endl;
+    std::cout << "I'm doing my best to bring you new features soon. Stay tuned!" << std::endl;
+    return 0; // Retorno 0 é geralmente melhor para "successo visual" mesmo que não faça nada
+}
 #else
 #include <kalidous/kalidous.hpp>
-    int main(int argc, const char** argv) {
-        return kalidous_run(argc, argv);
-    }
+
+int main(int argc, const char** argv) {
+    return kalidous_run(argc, argv);
+}
 #endif
