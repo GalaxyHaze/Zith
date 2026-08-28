4b8b55e00ede5e2055a38ee5f6bbb293ed91e66f
diogo <diogoalexandredecodasilvapt@gmail.com>
2026-08-28 19:48:51 +0100

    examples: replace examples with simple and advance feature samples

--- tests/test-examples.cpp
+++ tests/test-examples.cpp
@@ -20,12 +20,8 @@ struct Example {
 };
 
 constexpr Example kExamples[] = {
-    {"linked-list.zith", 7, false},  {"for-loops.zith", 10, false},
-    {"bitwise.zith", 0, false},      {"when-dispatch.zith", 20, false},
-    {"arrays.zith", 5, false},       {"nested-structs.zith", 4, false},
-    {"optionals.zith", 0, false},    {"generics-decl.zith", 3, false},
-    {"tagged-union.zith", 0, false}, {"dyn-interface.zith", 18, false},
-    {"c-interop.zith", 0, true},
+    {"simple-feature.zith", 0, false},
+    {"advance-feature.zith", 15, false},
 };
 
 #ifdef ZITH_ENABLE_C_INTEROP
